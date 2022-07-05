4052ec9c4a165b2a13ac38374e01e0a3b58b2e0a
gopigane <92424505+gopigane@users.noreply.github.com>
2022-07-05 13:02:15 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -23,7 +23,7 @@ resource "aws_vpc" "terraform-vpc" {
 resource "aws_subnet" "tera-sub-1" {
   vpc_id            = aws_vpc.terraform-vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "ap-south-1a"
   map_public_ip_on_launch = true
 tags = {
     Name = "ter-sub-1"
@@ -34,7 +34,7 @@ tags = {
 resource "aws_subnet" "tera-sub-2" {
   vpc_id            = aws_vpc.terraform-vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
+  availability_zone = "ap-south-1b"
  map_public_ip_on_launch = true  
 tags = {
     Name = "ter-sub-2"
